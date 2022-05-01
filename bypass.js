91cdb6f59b8eaaa71d61b5bde40fbe5ebe283b99
bcho892 <bcho892@aucklanduni.ac.nz>
2022-05-02 11:46:59 +1200

    simplified code

--- bypass.js
+++ bypass.js
@@ -3,12 +3,12 @@ function bypass(){
     var blocker = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-1kihuf0 r-18u37iz r-1pi2tsx r-1777fci r-1pjcn9w r-xr3zp9 r-1xcajam r-ipm5af r-g6jmlv")
         if(blocker === undefined){
            
-            return false;
+            return;
         }
         blocker[0].style["visibility"] = "hidden";
         document.documentElement.style["overflowY"] = "scroll";
 
-    return true;
+    return;
 }
 
 
@@ -19,10 +19,6 @@ chrome.action.onClicked.addListener((tab) => {
         function: bypass
       });
     }
-    else{
-        chrome.tabs.getSelected(null, function(tab) {
-            alert("Invalid Website (Not Twitter)")
-        })
-    }
+
   });
 

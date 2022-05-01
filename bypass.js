1b17360d7096ab54a7f046aed64874265a835e03
bcho892 <bcho892@aucklanduni.ac.nz>
2022-05-02 11:40:28 +1200

    added project files

---
+++ bypass.js
@@ -0,0 +1,28 @@
+
+function bypass(){
+    var blocker = document.getElementsByClassName("css-1dbjc4n r-1awozwy r-1kihuf0 r-18u37iz r-1pi2tsx r-1777fci r-1pjcn9w r-xr3zp9 r-1xcajam r-ipm5af r-g6jmlv")
+        if(blocker === undefined){
+           
+            return false;
+        }
+        blocker[0].style["visibility"] = "hidden";
+        document.documentElement.style["overflowY"] = "scroll";
+
+    return true;
+}
+
+
+chrome.action.onClicked.addListener((tab) => {
+    if(tab.url.includes("twitter")) {
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: bypass
+      });
+    }
+    else{
+        chrome.tabs.getSelected(null, function(tab) {
+            alert("Invalid Website (Not Twitter)")
+        })
+    }
+  });
+
